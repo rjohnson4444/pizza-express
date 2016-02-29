ef555661266df6f32ec600bac24f834cff4c2dd7
Ryan Johnson <rjohnson4444@live.com>
2016-02-29 09:41:15 -0700

    add pizza routes, tests, and views - add view for all pizzas and single pizza - add test for server and pizza views

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
-const express = require('express');
-const app = express();
+const express  = require('express');
+const app      = express();
+const bodyParser = require('body-parser');
+const generateId = require('./lib/generate-id');
+
+const path     = require('path');
+module.exports = app;
+
+app.use(express.static('static'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'jade');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pizza Express';
+app.locals.pizzas = {};
 
 app.get('/', (request, response) => {
-    response.send('Hello World!');
+    response.render('index');
 });
 
-app.listen(app.get('port'), () => {
-    console.log(`${app.locals.title} is running on ${app.get('port')}, `);
+app.get('/pizzas/:id', (request, response) => {
+    var pizza = app.locals.pizzas[request.params.id];
+
+    response.render('pizza', { pizza: pizza });
 });
+
+app.post('/pizzas', (request, response) => {
+    var id = generateId();
+
+    app.locals.pizzas[id] = request.body;
+
+    response.sendStatus(201);
+});
+
+if (!module.parent) {
+    app.listen(app.get('port'), () => {
+        console.log(`${app.locals.title} is running on ${app.get('port')}, `);
+    });
+}
